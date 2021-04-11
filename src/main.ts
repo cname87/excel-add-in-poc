fcd34210d139dd2337342ee95a84f6d70348a3e6
Seán A. Young <cname87@yahoo.com>
2021-04-11 18:36:05 +0100

    Fully local

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,32 @@ if (environment.production) {
   enableProdMode();
 }
 
-Office.initialize = (_reason: any) => {
+Office.onReady((info) => {
+  console.log(info);
+  if (info.host === Office.HostType.Excel) {
+      // Do Excel-specific initialization (for example, make add-in task pane's
+      // appearance compatible with Excel "green").
+  }
+  if (info.platform === Office.PlatformType.PC) {
+      // Make minor layout changes in the task pane.
+  }
+  console.log(`Office.js is now ready in ${info.host} on ${info.platform}`);
+});
+
+
+Office.initialize = (reason) => {
   //If you need to initialize something you can do so here. 
-  console.log('ReleasePlanner addin - Office is initialized');
+  console.log('Office is initialized');
+  console.log(`Reason in ${reason}`)
+
+  if (Office.context.requirements.isSetSupported('ExcelApi', '1.12'))
+{
+  console.log('ExcelAp1 v1.12 supported');
+}
+else
+{
+  console.log('ExcelApi v1.12 supported');
+}
   // Bootstrap the app
   platformBrowserDynamic()
       .bootstrapModule(AppModule)
